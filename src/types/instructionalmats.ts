f2ea158a47ee436e170320f0a3c0a253b41b0115
Myrone Silverio <silveriomyrone@gmail.com>
2025-09-09 02:42:38 +0800

    [WIP]: IM creation form

---
+++ src/types/instructionalmats.ts
@@ -0,0 +1,26 @@
+import type { UniversityIM } from "./universityim";
+import type { ServiceIM } from "./serviceim";
+
+export enum IMType {
+  university = "university",
+  service = "service",
+}
+
+export interface InstructionalMaterial {
+  id: number;
+  im_type: string;
+  university_im_id: number;
+  service_im_id: number;
+  status: string;
+  validity: string;
+  version: string;
+  s3_link: string;
+  notes?: string | null;
+  created_by: string;
+  created_at: string;
+  updated_by: string;
+  updated_at: string;
+  is_deleted: boolean;
+  university_im?: UniversityIM;
+  service_im?: ServiceIM;
+}
